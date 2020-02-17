41cd42508370aa6a48b2c56811a615aec1027a8f
dkkondo <58612684+dkkondo@users.noreply.github.com>
2020-02-16 22:41:21 -0600

    Update index.php
    tot2

--- assignment09/part01/index.php
+++ assignment09/part01/index.php
@@ -4,14 +4,16 @@
 </head>
 <body>
 <?php
-$weight = $'weight_1';
-$height = $'height_1';
+$weight = $_POST['weight_1'];
+$height = $_POST['height_1'];
 $BMI= ($weight/($height * $height)) * 703;
     
-<h1>Health Checky - BMI Calculator</h1>
-    <!-- Source: http://bmi-calculator.net/bmi-formula.php -->
+     //formula for calculation BMI;
+     //BMI = (Weight in Pounds / (Height in inches * Height in inches)) * 703
+     //Source: http:// www.bmi-calculator.net/bmi-formula.php -->
     ?>
-<p> id="output-here" your BMI is<?php echo $BMI; ?> ,which is considered:
+    <h1> BMI calculation </h1>
+<p id="output-here"> your BMI is<?php echo $BMI; ?> ,which is :
 <?php
 
 if($BMI <18){
